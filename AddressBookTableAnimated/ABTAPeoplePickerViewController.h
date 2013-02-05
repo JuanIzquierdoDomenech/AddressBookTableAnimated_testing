9afe92c1c060c15cc029e07a73ae1840316465b5
Juan Jesus Izquierdo Domenech <juanjesusizquierdodomenech@gmail.com>
2013-02-05 16:17:30 +0100

    Added ABPeoplePicker

---
+++ AddressBookTableAnimated/ABTAPeoplePickerViewController.h
@@ -0,0 +1,14 @@
+//
+//  ABTAPeoplePickerViewController.h
+//  AddressBookTableAnimated
+//
+//  Created by Juan Jesús Izquierdo Doménech on 05/02/13.
+//  Copyright (c) 2013 Juan Jesús Izquierdo Doménech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <AddressBookUI/AddressBookUI.h>
+
+@interface ABTAPeoplePickerViewController : UIViewController <ABPeoplePickerNavigationControllerDelegate>
+
+@end
---
+++ AddressBookTableAnimated/ABTAPeoplePickerViewController.m
@@ -0,0 +1,75 @@
+//
+//  ABTAPeoplePickerViewController.m
+//  AddressBookTableAnimated
+//
+//  Created by Juan Jesús Izquierdo Doménech on 05/02/13.
+//  Copyright (c) 2013 Juan Jesús Izquierdo Doménech. All rights reserved.
+//
+
+#import "ABTAPeoplePickerViewController.h"
+
+@interface ABTAPeoplePickerViewController ()
+
+@end
+
+@implementation ABTAPeoplePickerViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+    ABPeoplePickerNavigationController *peoplePiclerNavController = [[ABPeoplePickerNavigationController alloc] init];
+    peoplePiclerNavController.peoplePickerDelegate = self;
+    
+    [self presentViewController:peoplePiclerNavController
+                       animated:YES
+                     completion:nil];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - ABPeoplePickerNavigationControllerDelegate
+
+// Called after the user has pressed cancel
+// The delegate is responsible for dismissing the peoplePicker
+- (void)peoplePickerNavigationControllerDidCancel:(ABPeoplePickerNavigationController *)peoplePicker
+{
+    [peoplePicker dismissViewControllerAnimated:YES
+                                     completion:^(){
+                                         
+                                         [self.navigationController
+                                          popToRootViewControllerAnimated:YES];
+                                         
+                                     }];
+}
+
+// Called after a person has been selected by the user.
+// Return YES if you want the person to be displayed.
+// Return NO  to do nothing (the delegate is responsible for dismissing the peoplePicker).
+- (BOOL)peoplePickerNavigationController:(ABPeoplePickerNavigationController *)peoplePicker shouldContinueAfterSelectingPerson:(ABRecordRef)person;
+{
+    return NO;
+}
+
+// Called after a value has been selected by the user.
+// Return YES if you want default action to be performed.
+// Return NO to do nothing (the delegate is responsible for dismissing the peoplePicker).
+- (BOOL)peoplePickerNavigationController:(ABPeoplePickerNavigationController *)peoplePicker shouldContinueAfterSelectingPerson:(ABRecordRef)person property:(ABPropertyID)property identifier:(ABMultiValueIdentifier)identifier;
+{
+    return NO;
+}
+
+@end
